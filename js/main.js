a977363f92356368d546761fbd5638665dcb45f5
megancarson <megancarson_6@live.com>
2017-03-27 20:34:12 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-var myElement = document.querySelector("#body")
-
 function changeBackground(color) {
-   document.myElement.style.backgroundColor = color;   
+   document.p.style.backgroundColor = color;  
+   document.li.style.backgroundColor = color;  
+   document.h1.style.backgroundColor = color;  
+   document.h2.style.backgroundColor = color;  
+   document.h3.style.backgroundColor = color;  
 }
